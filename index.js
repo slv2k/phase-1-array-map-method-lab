e68d306fdd017b62787563306fe495ca89bcd1fa
samuel <samuelmmanor@gmail.com>
2022-01-12 19:06:50 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -12,5 +12,12 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  let eachTitle = tutorials.map(el => el.split(' '));
+  for (let word in eachTitle) {
+    for (let i = 0; i < eachTitle[word].length; i++) {
+      eachTitle[word][i] = eachTitle[word][i][0].toUpperCase() + eachTitle[word][i].substr(1);
+    }
+    tutorials[word] = eachTitle[word].join(' ');
+  }
+  return tutorials;
 }
